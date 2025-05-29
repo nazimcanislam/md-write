f64c5e03f2b3c60f47187c3a6b0b971c3dd87e7c
Nazımcan İslam <nazimcanislam@gmail.com>
2025-05-29 22:06:23 +0300

    Added offline support for PWA.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,28 @@
-const isDev: boolean = process.env.NODE_ENV === 'development';
+const isDev: boolean = process.env.NODE_ENV === "development";
 
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: isDev,
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 }, // 1 year
+      },
+    },
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offline-cache",
+        expiration: { maxEntries: 200, maxAgeSeconds: 30 * 24 * 60 * 60 }, // 30 days
+      },
+    },
+  ],
 });
 
 module.exports = withPWA({
